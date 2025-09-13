976dc01d67dbd70452a28ec910e5d014e363f6fc
kpuhger <krpuhger@gmail.com>
2025-09-12 18:21:12 -0700

    use uv instead of poetry

--- setup.py
+++ setup.py
@@ -1,62 +1,84 @@
 from setuptools import setup, find_packages
 import os
-import allensdk
 import re
+
+VERSION = "2.13.6"
+
 
 def parse_requirements_line(line):
     split = line.split("#")
     return split[0].strip()
 
+
 def parse_requirements_file(path):
-    with open(path, 'r') as fil:
+    with open(path, "r") as fil:
         return [parse_requirements_line(line) for line in fil.read().splitlines()]
 
+
 # http://bugs.python.org/issue8876#msg208792
-if hasattr(os, 'link'):
+if hasattr(os, "link"):
     del os.link
 
+
 def prepend_find_packages(*roots):
-    ''' Recursively traverse nested packages under the root directories
-    '''
+    """Recursively traverse nested packages under the root directories"""
     packages = []
-    
+
     for root in roots:
         packages += [root]
-        packages += [root + '.' + s for s in find_packages(root)]
-        
+        packages += [root + "." + s for s in find_packages(root)]
+
     return packages
 
-required = parse_requirements_file('requirements.txt')
 
-if os.environ.get('ALLENSDK_INTERNAL_REQUIREMENTS', 'false') == 'true':
-    required.extend(parse_requirements_file('internal_requirements.txt'))
+required = parse_requirements_file("requirements.txt")
 
-test_required = parse_requirements_file('test_requirements.txt')
+if os.environ.get("ALLENSDK_INTERNAL_REQUIREMENTS", "false") == "true":
+    required.extend(parse_requirements_file("internal_requirements.txt"))
+
+test_required = parse_requirements_file("test_requirements.txt")
 
 setup(
-    version = allensdk.__version__,
-    name = 'allensdk',
-    author = 'Allen Institute for Brain Science',
-    author_email = 'instituteci@alleninstitute.org',
-    packages = ['allensdk'],
-    include_package_data = True,
-    package_data={'allensdk': ['*.conf', '*.cfg', '*.md', '*.json', '*.dat', '*.env', '*.sh', '*.txt', 'bps', 'Makefile', 'LICENSE', '*.hoc', 'allensdk/brain_observatory/nwb/*.yaml'] },
-    description = 'core libraries for the allensdk.',
-    install_requires = required,
+    version=VERSION,
+    name="allensdk",
+    author="Allen Institute for Brain Science",
+    author_email="instituteci@alleninstitute.org",
+    packages=["allensdk"],
+    include_package_data=True,
+    package_data={
+        "allensdk": [
+            "*.conf",
+            "*.cfg",
+            "*.md",
+            "*.json",
+            "*.dat",
+            "*.env",
+            "*.sh",
+            "*.txt",
+            "bps",
+            "Makefile",
+            "LICENSE",
+            "*.hoc",
+            "allensdk/brain_observatory/nwb/*.yaml",
+        ]
+    },
+    description="core libraries for the allensdk.",
+    install_requires=required,
     tests_require=test_required,
-    setup_requires=['setuptools', 'jinja2>=2.7.3,<2.12.0', 'sphinx', 'numpydoc', 'pytest-runner'],
-    url='https://github.com/AllenInstitute/AllenSDK/tree/v%s' % (allensdk.__version__),
-    download_url = 'https://github.com/AllenInstitute/AllenSDK/tarball/v%s' % (allensdk.__version__),
-    keywords = ['neuroscience', 'bioinformatics', 'scientific'  ],
-    scripts=['allensdk/model/biophys_sim/scripts/bps'],
+    setup_requires=["setuptools", "jinja2>=2.7.3,<2.12.0", "sphinx", "numpydoc", "pytest-runner"],
+    url="https://github.com/AllenInstitute/AllenSDK/tree/v%s" % (VERSION),
+    download_url="https://github.com/AllenInstitute/AllenSDK/tarball/v%s" % (VERSION),
+    keywords=["neuroscience", "bioinformatics", "scientific"],
+    scripts=["allensdk/model/biophys_sim/scripts/bps"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'License :: Other/Proprietary License', # Allen Institute Software License
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6', 
-        'Programming Language :: Python :: 3.7', 
-        'Programming Language :: Python :: 3.8', 
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
-        ])
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: Other/Proprietary License",  # Allen Institute Software License
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
